2a3418df4346634ba841fc2391cbcdd927b251e4
Zlatko Fedor <zfedor@gmail.com>
2016-06-23 21:48:11 +0200

    first version

---
+++ src/index.js
@@ -0,0 +1,41 @@
+import { createElement } from 'react';
+import hoistNonReactStatics from 'hoist-non-react-statics';
+
+function createComponent(providerName, fn, propTypes, customStatics, Component) {
+  function Provider(...args) {
+    return createElement(Component, fn(...args));
+  }
+
+  const componentName = Component.displayName || Component.name;
+  Provider.displayName = `${componentName}${providerName}`;
+
+  if (propTypes) {
+    Component.propTypes = {
+      ...Component.propTypes,
+      ...propTypes,
+    };
+  }
+
+  hoistNonReactStatics(Provider, Component, customStatics);
+
+  return Provider;
+}
+
+export default function provideProps(providerName, fn, propTypes, customStatics) {
+  if (!fn) {
+    throw new Error('fn is undefined');
+  }
+
+  if (!providerName) {
+    throw new Error('Name is undefined');
+  }
+
+  return function provider(...args) {
+    // support for decorator pattern
+    if (args.length === 0) {
+      return (Component) => createComponent(providerName, fn, propTypes, customStatics, Component);
+    }
+
+    return createComponent(providerName, fn, propTypes, customStatics, ...args);
+  };
+}
